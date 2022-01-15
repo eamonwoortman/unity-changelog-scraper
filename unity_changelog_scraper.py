15add8cf0f1aabe39b940457d2b1abaef698e224
Eamon Woortman <eamon.woortman@gmail.com>
2022-01-15 13:57:52 +0100

    Added way of specifically testing a version or full or partial sets

--- unity_changelog_scraper.py
+++ unity_changelog_scraper.py
@@ -7,16 +7,25 @@ from version_scraper import find_unity_versions
 
 def test_scrapes(unity_versions: list[UnityVersion]):
     overwrite_output = True
+    test_full_set = False
     test_versions = [
 #       '2022.1.0.13', 
 #       '2021.1.21', 
 #       '2021.2.5', 
+#       '5.1.2',
 #       '5.6.4',
-        '5.2.1',
+#        '5.2.1',
 #        '2021.2.0'    
+        '2018.2.4'
     ]
-    
-    version_objects = unity_versions #(x for x in unity_versions if any(x.version_string == w for w in test_versions)) # filter(lambda x: all(x.version_string in g for g in test_versions) in x, unity_versions)
+    test_specific_version = len(test_versions) > 0
+    if test_specific_version:
+        version_objects = (x for x in unity_versions if any(x.version_string == w for w in test_versions))
+    elif test_full_set:
+        version_objects = unity_versions
+    else:
+        version_objects = unity_versions[slice(10, len(unity_versions), 5)]
+
     for version in version_objects:
         scrape_changelog_version(version, overwrite_output)
 
@@ -26,12 +35,14 @@ def test_scrapes(unity_versions: list[UnityVersion]):
     write_catalog(unity_versions)
 
 def main():
-    #clear our output folder
+    print_versions = False
+    # clear our output folder
     clear_output_folder()
 
     # get unity versions
     unity_versions = find_unity_versions()
-    print([x.name for x in unity_versions])
+    if print_versions:
+        print([x.name for x in unity_versions])
 
     # scrape each changelog page
     test_scrapes(unity_versions)
