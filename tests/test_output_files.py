0780e3d8d9f177e0b4c5a608712974114fe41118
Eamon Woortman <eamon.woortman@gmail.com>
2022-01-15 21:01:36 +0100

    Added more criteria to the category type test

--- tests/test_output_files.py
+++ tests/test_output_files.py
@@ -1,11 +1,11 @@
 import json
-import unittest
 from pathlib import Path
-from unittest import TestCase, mock
+from unittest import TestCase
 
 from ddt import ddt, idata
 
 
+# wrapper class to allow asignment of __name__
 class NamedDict(dict):
     pass
 
@@ -15,7 +15,7 @@ def get_file_data(glob_pattern):
     for file in Path(__file__).parent.glob(glob_pattern):
         if 'catalog' in file.name:
             continue
-        with open(file) as reader:
+        with open(file, mode="r", encoding="utf-8") as reader:
             json_data = json.load(reader)
             dataitem = NamedDict(json_data)
             setattr(dataitem, '__name__', file.name)
@@ -29,14 +29,13 @@ class TestFileDatas(TestCase):
     @idata(get_file_data('../output/*.json'))
     def test_file_data(self, changelog):
         self.assert_categories_types_valid(changelog)
-
+        return True
 
     def assert_category_type_valid(self, category_type):
         self.longMessage = True
         self.assertFalse(not category_type, 'Category type is empty')
-        #self.assertFalse(not category_type, 'Category type: "%s"'%category_type)
-        
-        return True
+        self.assertLess(len(category_type), 50,'Category type is too long: %s(%d)'%(category_type, len(category_type)))
+        self.assertLess(len(category_type.split()), 4, 'Category type is too long: %s(%d)'%(category_type, len(category_type)))
 
     def assert_categories_types_valid(self, changelog):
         categories_types = changelog['category_types']
