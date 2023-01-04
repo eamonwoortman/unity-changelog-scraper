66fec7da9d3c84dfd9fd3975678942aa21c7a1ff
Eamon Woortman <eamon.woortman@gmail.com>
2023-01-04 14:57:55 +0100

    Updated regex to be a bit more precise

--- helpers/unity_version.py
+++ helpers/unity_version.py
@@ -1,22 +1,27 @@
 import re
 
-from slugify import slugify
-
+VERSION_REGEX_PATTERN = r'(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(?:(?P<prerelease>[fab]\d+))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?'
 
 def versiontuple(v):
     if isinstance(v, str):
         return tuple(map(int, (v.split("."))))
     return tuple(map(int, v))
 
-def parse_version_tuple(version_string): 
-    regex_match = re.match('^([0-9]+)\.([0-9]+)(?:\.([0-9]+))?(?:[abf]([0-9A-Za-z-]+(?:\.[0-9A-Za-z-]+)*))?(?:\+[0-9A-Za-z-]+)?', version_string)
+def parse_unity_version(version_string):
+    version_match = re.search(VERSION_REGEX_PATTERN, version_string)
+    if (version_match is None or len(version_match.groups()) == 0):
+        return None
+    return version_match.group(0)
+
+def parse_version_tuple(version_string):
+    regex_match = re.search(VERSION_REGEX_PATTERN, version_string)
     return versiontuple(filter(lambda x: x is not None, regex_match.groups()))
 
 class UnityVersion:
     def __init__(self, name, url):
         self.name = name
         self.version_string = url.rsplit('/', 1)[-1].replace('unity-', '')
-        self.file_name = '%s.json'%self.version_string
+        self.file_name = f'{self.version_string}.json'
         self.url = url
         self.is_valid = self.parse_version_object()
 
